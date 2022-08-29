e5d4f789606057d4f6518f9043b6cee13bf52120
Marcos <kerallis2015@gmail.com>
2022-08-29 12:15:39 -0300

    .

--- credito.js
+++ credito.js
@@ -1,22 +1,23 @@
 const calcularCredito = (salario, dependentes, emprestimo, parcelas) => {
 
-    if(!salario || !dependentes || !emprestimo || !parcelas) return "Erro"
-    if(salario == NaN || dependentes == NaN  || emprestimo == NaN  || parcelas == NaN ) return "Erro"
+    if(!salario || dependentes == undefined || !emprestimo || !parcelas) return "Erro - campo vazio"
+    
+    if(!isNumeric(salario) || !isNumeric(dependentes)
+    || !isNumeric(emprestimo) || !isNumeric(parcelas)) return "Erro - um dos campos não é número"
 
-    if(salario <= 1212 || dependentes < 0 || emprestimo <= 0 || parcelas > 36 || parcelas < 1) return "Erro"
+    if(salario <= 1212 || dependentes < 0 || emprestimo <= 0 || parcelas > 36 || parcelas < 1) return "Erro - limite mínimo ou máximo atingido"
 
-    if((parcelas - Math.floor(parcelas)) !== 0) return "Erro" // números decimais
-    if((dependentes - Math.floor(dependentes)) !== 0) return "Erro" // números decimais
+    if((parcelas - Math.floor(parcelas)) !== 0) return "Erro - numeros decimais em campos inteiros" // números decimais
+    if((dependentes - Math.floor(dependentes)) !== 0) return "Erro - numeros decimais em campos inteiros" // números decimais
 
-    const resultado = ((salario / (dependentes + 1)) / (emprestimo / parcelas)) * 100
-
+    const resultado =  ((emprestimo / parcelas) / (salario / (dependentes + 1))) * 100
     if(resultado > 100) {
-        return "Erro"
+        return "Erro - excedeu 100%"
     } else if(resultado < 0) {
-        return "Erro"
+        return "Erro - abaixo de 0%"
     }else if(resultado >= 30) {
         return "Ruim"
-    }else if(resultado < 30) {
+    }else if(resultado < 30 && resultado >= 10) {
         return "Bom"
     }else if(resultado < 10) {
         return "Ótimo"
@@ -25,5 +26,10 @@ const calcularCredito = (salario, dependentes, emprestimo, parcelas) => {
     return "Não foi possível calcular"
 
 }
+
+function isNumeric(str) {
+    if (typeof str == "number") return true
+    return !isNaN(str)
+  }
 
 module.exports = {calcularCredito}
--- credito.test.js
+++ credito.test.js
@@ -4,33 +4,39 @@ describe("calcular qualidade do emprestimo", () => {
 
     it("expectativa de erros", () => {
 
-        expect(calcularCredito(undefined, 0, 12_000, 24)).toBe('Erro')
-        expect(calcularCredito(8_000, undefined, 8_000, 24)).toBe('Erro')
-        expect(calcularCredito(8_000, 0, undefined, 24)).toBe('Erro')
-        expect(calcularCredito(8_000, 0, 3_000, undefined)).toBe('Erro')
-        expect(calcularCredito(12_000, 'A', 5_000, 4)).toBe('Erro')
-        expect(calcularCredito(12_000, 1.5, 5_000, 4)).toBe('Erro')
-        expect(calcularCredito(12_000, 0, 5_000, 4.2)).toBe('Erro')
-        expect(calcularCredito(-5_000, 2, 2_000, 12)).toBe('Erro')
-        expect(calcularCredito(5_000, -2, 2_000, 12)).toBe('Erro')
-        expect(calcularCredito(5_000, 2, -2_000, 12)).toBe('Erro')
-        expect(calcularCredito(5_000, 2, 2_000, -12)).toBe('Erro')
+        expect(calcularCredito(undefined, 0, 12000, 24)).toBe('Erro - campo vazio')
+        expect(calcularCredito(8000, undefined, 8000, 24)).toBe('Erro - campo vazio')
+        expect(calcularCredito(8000, 0, undefined, 24)).toBe('Erro - campo vazio')
+        expect(calcularCredito(8000, 0, 3000, undefined)).toBe('Erro - campo vazio')
+        expect(calcularCredito(12000, 'A', 5000, 4)).toBe('Erro - um dos campos não é número')
+        expect(calcularCredito(500, 2, 2000, -12)).toBe('Erro - limite mínimo ou máximo atingido')
+        expect(calcularCredito(12000, 1.5, 5000, 4)).toBe('Erro - numeros decimais em campos inteiros')
+        expect(calcularCredito(12000, 0, 5000, 4.2)).toBe('Erro - numeros decimais em campos inteiros')
+        expect(calcularCredito(-5000, 2, 2000, 12)).toBe('Erro - limite mínimo ou máximo atingido')
+        expect(calcularCredito(5000, -2, 2000, 12)).toBe('Erro - limite mínimo ou máximo atingido')
+        expect(calcularCredito(5000, 2, -2000, 12)).toBe('Erro - limite mínimo ou máximo atingido')
+        expect(calcularCredito(5000, 2, 2000, -12)).toBe('Erro - limite mínimo ou máximo atingido')
+        
+    })
+
+    it("expectativa com ótimo", () => {
+
+        expect(calcularCredito(15000, 1, 12000, 17)).toBe('Ótimo') // 9.4
+        expect(calcularCredito(7000, 0, 16000, 30)).toBe('Ótimo') // 7.6
     
     })
 
-    it("expectativa com ótimos", () => {
+    it("expectativa com bom", () => {
 
-        expect(calcularCredito(undefined, 0, 12_000, 24)).toBe('Erro')
-        expect(calcularCredito(8_000, undefined, 8_000, 24)).toBe('Erro')
-        expect(calcularCredito(8_000, 0, undefined, 24)).toBe('Erro')
-        expect(calcularCredito(8_000, 0, 3_000, undefined)).toBe('Erro')
-        expect(calcularCredito(12_000, 'A', 5_000, 4)).toBe('Erro')
-        expect(calcularCredito(12_000, 1.5, 5_000, 4)).toBe('Erro')
-        expect(calcularCredito(12_000, 0, 5_000, 4.2)).toBe('Erro')
-        expect(calcularCredito(-5_000, 2, 2_000, 12)).toBe('Erro')
-        expect(calcularCredito(5_000, -2, 2_000, 12)).toBe('Erro')
-        expect(calcularCredito(5_000, 2, -2_000, 12)).toBe('Erro')
-        expect(calcularCredito(5_000, 2, 2_000, -12)).toBe('Erro')
+        expect(calcularCredito(12000, 1, 8000, 7)).toBe('Bom') // 19
+        expect(calcularCredito(12000, 1, 8500, 5)).toBe('Bom') // 28.3
+    
+    })
+
+    it("expectativa com ruim", () => {
+
+        expect(calcularCredito(6000, 1, 10000, 6)).toBe('Ruim') // 55.5
+        expect(calcularCredito(6200, 1, 8500, 9)).toBe('Ruim') // 30.4
     
     })
 
